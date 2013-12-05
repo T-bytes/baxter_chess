28d67eb3d19c53a9781d156fcdae9149cdda5fb3
Trevor M. Sands <TMSands@users.noreply.github.com>
2013-12-05 14:38:44 -0500

    Update MoveArmsIK.py

--- MoveArmsIK.py
+++ MoveArmsIK.py
@@ -27,10 +27,6 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-"""
-Baxter RSDK Joint Position Example: file playback
-"""
-
 import argparse
 import sys
 
@@ -66,21 +62,12 @@ def inverse_kinematics(limb, x_p, y_p, z_p, x_o, y_o, z_o, w_o):
 					x = x_p,
 					y = y_p,
 					z = z_p,
-					# EXAMPLE:
-					# x=0.657579481614,
-					# y=0.851981417433,
-					# z=0.0388352386502,
 				),
 				orientation=Quaternion(
 					x = x_o,
 					y = y_o,
 					z = z_o,
 					w = w_o,
-					# EXAMPLE:
-					# x=-0.366894936773,
-					# y=0.885980397775,
-					# z=0.108155782462,
-					# w=0.262162481772,
 				),
 			),
 		),
@@ -91,21 +78,12 @@ def inverse_kinematics(limb, x_p, y_p, z_p, x_o, y_o, z_o, w_o):
 					x = x_p,
 					y = y_p,
 					z = z_p,
-					# EXAMPLE:
-					# x=0.656982770038,
-					# y=-0.852598021641,
-					# z=0.0388609422173,
 				),
 				orientation=Quaternion(
 					x = x_o,
 					y = y_o,
 					z = z_o,
 					w = w_o,
-					# EXAMPLE:
-					# x=0.367048116303,
-					# y=0.885911751787,
-					# z=-0.108908281936,
-					# w=0.261868353356,
 				),
 			),
 		),
@@ -177,19 +155,12 @@ if __name__ == '__main__':
 	left.move_to_neutral()
 	right.move_to_neutral()
 	
-	# Print left arm end effector position and orientation
-	print(left.endpoint_pose())
-	
-	# =======================
-	# TEST FORWARD KINEMATICS
-	# =======================
-	
-	"""
-	lcmd_start = {'left_w0': 0.0249271877747, 'left_w1': -0.0563737938904, 'left_w2': 0.12386894848, 'left_e0': 0.08628641922, 'left_e1': 2.45091780104, 'left_s0': -0.616660276025, 'left_s1': -0.807640883899}
-	rcmd_start = {'right_s0': 1.01702926121, 'right_s1': 0.185611675122, 'right_w0': 0.359718494348, 'right_w1': -0.0421844716187, 'right_w2': -0.469781615753, 'right_e0': -0.034514567688, 'right_e1': 1.44002446298}
-	left.move_to_joint_positions(lcmd_start)
-	right.move_to_joint_positions(rcmd_start)
-	"""
+	# Print end effector position and orientation
+	print("End effector position:")
+	if (limb == 'left'):
+		print(left.endpoint_pose())
+	if (limb == 'right'):
+		print(right.endpoint_pose())
 	
 	# =======================
 	# TEST INVERSE KINEMATICS
