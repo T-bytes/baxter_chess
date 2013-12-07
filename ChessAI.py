bc739a81816f4159dd4bcf9c10309b75e5b8430a
Trevor M. Sands <TMSands@users.noreply.github.com>
2013-12-07 14:09:19 -0500

    Update ChessAI.py

--- ChessAI.py
+++ ChessAI.py
@@ -8,7 +8,7 @@
  http://yakinikuman.wordpress.com/
  
  Edited by:
- 	Trevor Sands for EEEE 585, Rochester Institute of Technology under GNU License
+ 	Trevor Sands for EEEE 585, Rochester Institute of Technology under GNU GPL
  	December 7, 2013
  """
  
