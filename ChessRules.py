09cb902d717d4e53941e1267101dd1e9423ebd37
Trevor M. Sands <TMSands@users.noreply.github.com>
2013-12-07 14:08:08 -0500

    Update ChessRules.py

--- ChessRules.py
+++ ChessRules.py
@@ -6,6 +6,10 @@
 	
  Copyright (C) 2009 Steve Osborne, srosborne (at) gmail.com
  http://yakinikuman.wordpress.com/
+ 
+ Edited by:
+ 	Trevor Sands for EEEE 585, Rochester Institute of Technology under GNU License
+ 	December 7, 2013
  """
  
 class ChessRules:
